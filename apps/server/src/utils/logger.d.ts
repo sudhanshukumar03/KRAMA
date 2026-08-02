d69a2930350aa310dc15f19192d55e4dea5e0e76
sudhanshu <sksingh032003@gmail.com>
2026-08-02 06:26:41 +0530

    chore: update apps/server/src/utils/logger.d.ts

---
+++ apps/server/src/utils/logger.d.ts
@@ -0,0 +1,6 @@
+export declare const logger: {
+    info: (msg: string, meta?: Record<string, any>) => void;
+    warn: (msg: string, meta?: Record<string, any>) => void;
+    error: (msg: string, meta?: Record<string, any>) => void;
+};
+//# sourceMappingURL=logger.d.ts.map
