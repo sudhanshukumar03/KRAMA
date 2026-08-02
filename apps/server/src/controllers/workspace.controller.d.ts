337796c16b656fa2bfbeb5125e0f1bec04559beb
sudhanshu <sksingh032003@gmail.com>
2026-08-02 06:25:59 +0530

    chore: update apps/server/src/controllers/workspace.controller.d.ts

--- apps/server/src/controllers/workspace.controller.d.ts
+++ apps/server/src/controllers/workspace.controller.d.ts
@@ -4,4 +4,5 @@ export declare const getWorkspace: (req: Request, res: Response) => Promise<Resp
 export declare const createWorkspace: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 export declare const updateWorkspace: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 export declare const deleteWorkspace: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
+export declare const exportWorkspace: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 //# sourceMappingURL=workspace.controller.d.ts.map
