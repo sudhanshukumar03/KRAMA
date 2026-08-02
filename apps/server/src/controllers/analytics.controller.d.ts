c34da70d38aa3b6ee4e3329114a5f9d01d703c57
sudhanshu <sksingh032003@gmail.com>
2026-08-02 06:25:44 +0530

    chore: update apps/server/src/controllers/analytics.controller.d.ts

---
+++ apps/server/src/controllers/analytics.controller.d.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from 'express';
+export declare const getOverview: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
+export declare const getFocusHistory: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
+export declare const getHabitHeatmap: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
+//# sourceMappingURL=analytics.controller.d.ts.map
