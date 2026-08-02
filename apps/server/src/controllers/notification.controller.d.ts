bab79328a31634991f204bd5555d1b6a3d3ca8c0
sudhanshu <sksingh032003@gmail.com>
2026-08-02 06:25:52 +0530

    chore: update apps/server/src/controllers/notification.controller.d.ts

---
+++ apps/server/src/controllers/notification.controller.d.ts
@@ -0,0 +1,4 @@
+import type { Request, Response } from 'express';
+export declare const getNotifications: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
+export declare const markAsRead: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
+//# sourceMappingURL=notification.controller.d.ts.map
