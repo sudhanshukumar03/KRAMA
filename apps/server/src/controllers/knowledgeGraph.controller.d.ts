533855842afc81e53409c5fe07ece5dcee2392a5
sudhanshu <sksingh032003@gmail.com>
2026-08-02 06:25:51 +0530

    chore: update apps/server/src/controllers/knowledgeGraph.controller.d.ts

---
+++ apps/server/src/controllers/knowledgeGraph.controller.d.ts
@@ -0,0 +1,3 @@
+import type { Request, Response } from 'express';
+export declare const getKnowledgeGraph: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
+//# sourceMappingURL=knowledgeGraph.controller.d.ts.map
