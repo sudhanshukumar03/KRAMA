cbbebebffa90eb02337423aa81a6217d08f3e5d5
sudhanshu <sksingh032003@gmail.com>
2026-08-02 06:25:40 +0530

    chore: update apps/server/run-migrate.cjs

--- apps/server/run-migrate.cjs
+++
@@ -1,8 +0,0 @@
-const { spawn } = require('child_process');
-const child = spawn('npx', ['prisma', 'migrate', 'dev', '--name', 'add_auth'], { shell: true });
-child.stdout.pipe(process.stdout);
-child.stderr.pipe(process.stderr);
-setTimeout(() => {
-  child.stdin.write('y\n');
-  child.stdin.end();
-}, 2000);
