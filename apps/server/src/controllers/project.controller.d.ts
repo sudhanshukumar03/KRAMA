765c636b9198b684efb15bb67b67a4df7559dfca
sudhanshu <sksingh032003@gmail.com>
2026-08-02 06:25:54 +0530

    chore: update apps/server/src/controllers/project.controller.d.ts

--- apps/server/src/controllers/project.controller.d.ts
+++ apps/server/src/controllers/project.controller.d.ts
@@ -5,4 +5,5 @@ export declare const createProject: (req: Request, res: Response) => Promise<Res
 export declare const updateProject: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 export declare const deleteProject: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 export declare const reorderProject: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
+export declare const restoreProject: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 //# sourceMappingURL=project.controller.d.ts.map
