9efd29af280381d7621020548caa5ac2d1e75424
sudhanshu <sksingh032003@gmail.com>
2026-08-02 06:25:53 +0530

    chore: update apps/server/src/controllers/page.controller.d.ts

--- apps/server/src/controllers/page.controller.d.ts
+++ apps/server/src/controllers/page.controller.d.ts
@@ -4,4 +4,5 @@ export declare const getPage: (req: Request, res: Response) => Promise<Response<
 export declare const createPage: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 export declare const updatePage: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 export declare const deletePage: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
+export declare const restorePage: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 //# sourceMappingURL=page.controller.d.ts.map
