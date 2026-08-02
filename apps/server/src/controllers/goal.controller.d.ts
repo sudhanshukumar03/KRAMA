053759f5babbe5a91a669419fdede7786a174924
sudhanshu <sksingh032003@gmail.com>
2026-08-02 06:25:49 +0530

    chore: update apps/server/src/controllers/goal.controller.d.ts

--- apps/server/src/controllers/goal.controller.d.ts
+++ apps/server/src/controllers/goal.controller.d.ts
@@ -4,4 +4,5 @@ export declare const getGoal: (req: Request, res: Response) => Promise<Response<
 export declare const createGoal: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 export declare const updateGoal: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 export declare const deleteGoal: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
+export declare const restoreGoal: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
 //# sourceMappingURL=goal.controller.d.ts.map
