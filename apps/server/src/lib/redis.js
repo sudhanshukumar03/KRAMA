76817efdc5ee131c75a157acf6920094106841ac
sudhanshu <sksingh032003@gmail.com>
2026-08-02 06:26:03 +0530

    chore: update apps/server/src/lib/redis.js

--- apps/server/src/lib/redis.js
+++
@@ -1,8 +0,0 @@
-import Redis from 'ioredis';
-const REDIS_URL = process.env.REDIS_URL || 'redis://:krama_redis_secret@localhost:6379';
-// Shared connection for BullMQ
-// We pass maxRetriesPerRequest: null as required by BullMQ
-export const connection = new Redis(REDIS_URL, {
-    maxRetriesPerRequest: null,
-});
-//# sourceMappingURL=redis.js.map
